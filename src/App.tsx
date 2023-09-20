b954e8f501c6e073a5496fe53f9f18051abe311a
Kirill Vanyukov <35896475+vanyukov@users.noreply.github.com>
2023-09-20 18:53:04 +0530

    initial script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {}, [])
   return (
     <div className="App">
-      <img src="/images/logo/logo_57x57.png" alt="KV logo" />
+      <img
+        src="//helper.kvanyukov.ru/images/logo/logo_57x57.png"
+        alt="KV logo"
+        width={32}
+        height={32}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
